35f0a1d75afe10dbd20975784afad01064805026
Khalefa <m.elbasiouny@gmail.com>
2012-08-13 03:12:52 +0200

    ex2

---
+++ figs/expr/convert.bat
@@ -0,0 +1,15 @@
+ps2pdf	g_forecast.eps
+ps2pdf	g_forecast_AA.eps
+ps2pdf	g_forecast_MA_l0.eps
+ps2pdf	g_forecast_MA_l1.eps
+ps2pdf	g_interval_l0.eps
+ps2pdf	g_interval_l1.eps
+ps2pdf	g_interval_l2.eps
+ps2pdf	g_layers_i10.eps
+ps2pdf	g_layers_i100.eps
+ps2pdf	g_layers_i2.eps
+ps2pdf	g_scall0i1.eps
+ps2pdf	g_scal_l0i100.eps
+ps2pdf	g_scal_l1i1.eps
+ps2pdf	g_scal_l1i10.eps
+ps2pdf	g_scal_l1i2.eps
